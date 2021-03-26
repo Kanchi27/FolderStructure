06943463ede2c2de8ffa1ad161b4e49161d27051
Kanchi27 <43461269+Kanchi27@users.noreply.github.com>
2021-03-26 16:41:44 +0530

    Add files via upload

--- src/components/FolderStructure.js
+++ src/components/FolderStructure.js
@@ -1,11 +1,22 @@
 import {useState} from 'react'
 
 const FolderStructure = ({data}) => {
-  
+    const toggleDisplay = (e) => {
+        const el = e.target;
+        console.log(el)
+        const sibling = el.nextSibling;
+        /* if (el && el.className == 'toggle' && sibling) {
+            sibling.classList.toggle('show');
+        } */
+        if (el && el.className == 'toggle') {
+            el.classList.toggle('hide')
+        }
+        console.log(el)
+    }
     return (
-        <ul>
+        <ul className="folder-str">
         {data.map((curr, index) => (
-          <li key={index} onClick={() => console.log(index)}>
+          <li key={index} onClick={toggleDisplay}>
             {curr.title}
             {curr.children && (
               <FolderStructure data={curr.children} />  
